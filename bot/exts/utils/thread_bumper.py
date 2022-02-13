2d8fa806aff55e5efdbd6b74440acf78c256fdd5
Chris Lovering <chris.lovering.95@gmail.com>
2022-02-13 23:34:43 +0000

    Add logic so that manaully archived threads bypass the thread bump list

--- bot/exts/utils/thread_bumper.py
+++ bot/exts/utils/thread_bumper.py
@@ -23,6 +23,40 @@ class ThreadBumper(commands.Cog):
         self.bot = bot
         self.init_task = scheduling.create_task(self.ensure_bumped_threads_are_active(), event_loop=self.bot.loop)
 
+    async def unarchive_if_not_manually_archived(self, thread: discord.Thread) -> None:
+        """
+        Unarchive a thread if it wasn't manually archived.
+
+        Compare the difference between the thread's last message timestamp
+        and the thread archive time with when it was actually archived.
+
+        If the difference is within a minute, assume it was auto archived, and bump it back open.
+
+        If the difference is greater than a minute, assume it was manually archived,
+        leave it archived, and remove it from the bumped threads list.
+        """
+        if not thread.archived:
+            return
+        messages = await thread.history(limit=1).flatten()
+        last_message = messages[0]
+        if not isinstance(last_message, discord.Message):
+            return
+
+        last_message_til_archive_time = (thread.archive_timestamp - last_message.created_at).total_seconds()
+        difference = abs(last_message_til_archive_time*60 - thread.auto_archive_duration)
+
+        if difference < 1:
+            # If the difference is less than 1 minute, it means the thread was auto-archived.
+            await thread.edit(archived=False)
+            return
+
+        log.info(
+            "#%s (%d) was manually archived. Leaving archived, and removing from bumped threads.",
+            thread.name,
+            thread.id
+        )
+        await self.threads_to_bump.delete(thread.id)
+
     async def ensure_bumped_threads_are_active(self) -> None:
         """Ensure bumped threads are active, since threads could have been archived while the bot was down."""
         await self.bot.wait_until_guild_available()
@@ -38,7 +72,7 @@ class ThreadBumper(commands.Cog):
                 log.info(f"Thread {thread_id} has been deleted, removing from bumped threads.")
                 await self.threads_to_bump.delete(thread_id)
             if thread.archived:
-                await thread.edit(archived=False)
+                await self.unarchive_if_not_manually_archived(thread)
 
     @commands.group(name="bump")
     async def thread_bump_group(self, ctx: commands.Context) -> None:
@@ -100,7 +134,7 @@ class ThreadBumper(commands.Cog):
 
         bumped_threads = [k for k, _ in await self.threads_to_bump.items()]
         if after.id in bumped_threads:
-            await after.edit(archived=False)
+            await self.unarchive_if_not_manually_archived(after)
 
     async def cog_check(self, ctx: commands.Context) -> bool:
         """Only allow staff & partner roles to invoke the commands in this cog."""
